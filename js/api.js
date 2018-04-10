d9c284321df92f2c19d20248e445c9cdd6fbbb27
Anuja Supekar <anuja@Anujas-MacBook-Pro.local>
2018-04-10 16:21:34 -0700

    Get gifs based on the search and load more as requested

--- js/api.js
+++ js/api.js
@@ -3,6 +3,13 @@ var api = {
 		$.get('http://api.giphy.com/v1/gifs/trending', { api_key : _apiKey }, function(data) {
 	    	callback(data);
 		});
+	},
+
+	getSearched : function(_apiKey, _searchTerm, callback) {
+		$.get('http://api.giphy.com/v1/gifs/search', { api_key : _apiKey, q : _searchTerm }, function(data) {
+			console.log(data);
+	    	callback(data);
+		});
 	}
 }
 
--- js/main.js
+++ js/main.js
@@ -5,15 +5,38 @@ function main(){
 	api.getAllTrending(api_key, gotAllTrending);
 }
 
-function gotAllTrending(trendingGifs) {
+function gotAllTrending(trendingGifs, isSearchQuery) {
 	for(var i=0; i<trendingGifs["data"].length; i++) {
 		var gif_data = trendingGifs["data"][i]["images"]["original"];
 		render.renderGif(gif_data);
 	}
+
+	if(isSearchQuery) {
+		$(".gifsContainer").append('<button id="loadBtn">Load More</button>');
+	}
+
+	$("#loadBtn").click(function() {
+		console.log("load more pressed");
+		$("#loadBtn").hide();
+		getMoreGifsBasedOnSearch();
+	});	
+
+	$("#searchBtn").click(function() {
+		$(".gifsContainer").html('');
+		console.log("button pressed");
+		getGifsBasedOnSearch();
+	});
 }
 
-function gotOneTrending(gif) {
-	
-
+function getGifsBasedOnSearch() {
+	var searchTerm = $("#searchBox").val();
+	api.getSearched(api_key, searchTerm, gotSearchedGifs);
 }
 
+function gotSearchedGifs(searchedGifs) {	
+	gotAllTrending(searchedGifs, true);
+}
+
+function getMoreGifsBasedOnSearch() {
+	getGifsBasedOnSearch();
+}
